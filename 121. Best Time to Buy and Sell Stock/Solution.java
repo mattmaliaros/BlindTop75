e580c781928bb2dc5c4b66ece84377c0f32882ef
MATTHEW MALIAROS <42752582+mattmaliaros@users.noreply.github.com>
2021-04-21 16:45:39 -0400

    Update Solution.java

--- 121. Best Time to Buy and Sell Stock/Solution.java
+++ 121. Best Time to Buy and Sell Stock/Solution.java
@@ -1,13 +1,22 @@
 public class Solution {
     public int maxProfit(int prices[]) {
-        int maxprofit = 0;
+        // Create final result as maxProfit
+        int maxProfit = 0;
+        
+        //Two for loops -- This is not the optimal solution, this is a O(n^2) solution
         for (int i = 0; i < prices.length - 1; i++) {
             for (int j = i + 1; j < prices.length; j++) {
+                
+                // Create a profit variable every iteration of the inner loop.
+                // The code below is essentially a Math.max function
+                // If we find a profit greater than the maxProfit, reassign the maxProfit.
                 int profit = prices[j] - prices[i];
-                if (profit > maxprofit)
-                    maxprofit = profit;
+                if (profit > maxProfit)
+                    maxProfit = profit;
             }
         }
-        return maxprofit;
+        
+        // Return the maximum 
+        return maxProfit;
     }
 }
