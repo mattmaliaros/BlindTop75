3e9a668df6d1e8bff4918a944e97f55ce121044d
MATTHEW MALIAROS <42752582+mattmaliaros@users.noreply.github.com>
2021-04-22 09:57:52 -0400

    Update Solution.java

--- 70. Climbing Stairs/Solution.java
+++ 70. Climbing Stairs/Solution.java
@@ -1,8 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
-        // This is a tabulation solve.  We are only keeping 3 entries in the table and updating them when necessary.
+        // This is a tabulation solve.  EDIT: We are calculating all entries in the table by hardcoding the first two values and then calculating each value after.
         // Setting the first two values before checking our base cases.  This way, we can also solve for 3, 4, 5... n, etc.
-        
+        // We will have a full table of n entries where table[n] is always going to be equal to table[n-1] + table[n-2] -- See line 16.
         
         int[] table = new int[n+1];
         table[1] = 1; 
